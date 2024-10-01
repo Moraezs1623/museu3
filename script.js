4632addfc8d78bc90922bfa4e3732fb4ada3820c
Gustavo de Moraes <gtvmoraes1623@gmail.com>
2024-10-01 16:45:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function confirmLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    alert(`Login realizado com:\nNome: ${username}\nSenha: ${password}`);
+    window.location.href = 'index.html';
+}
+
+function openImagePopup() {
+   
+    alert("");
+}
